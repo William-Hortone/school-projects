421432d761e234bf976c9042d7f8aa05eb744b25
William Hortone <willhortone@gmail.com>
2023-11-08 01:58:34 +0800

    updated the links

--- hospital-management-system/fontend/src/pages/admin/home/HomeAdmin.jsx
+++ hospital-management-system/fontend/src/pages/admin/home/HomeAdmin.jsx
@@ -20,14 +20,11 @@ const HomeAdmin = () => {
         <Link to="/vHospital" className="admin-category">
           View hospital services details
         </Link>
-        <Link to="/vHospital" className="admin-category">
-          View hospital services details
         <Link to="/doctorApp" className="admin-category">
           Doctor Appointments
         </Link>
         <Link to="/serviceSchedule" className="admin-category">
           Service Scheduling
-
         </Link>
       </div>
     </div>
