0d0d45fb98f4d0a79d14999fcd5920af4eca5e19
William Hortone <willhortone@gmail.com>
2023-12-05 05:46:15 +0800

    added the out patient & doctor table  details

--- hospital-management-system/frontend/src/containers/patient/addDocAppointment/AddDocAppointment.jsx
+++ hospital-management-system/frontend/src/containers/patient/addDocAppointment/AddDocAppointment.jsx
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { Input, Select } from "../../../components";
+import { ButtonAction, Input, Select } from "../../../components";
 import axios from "axios";
 import "./addDocAppointment.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+// import DatePicker from 'react-datepicker';
+// import 'react-datepicker/dist/react-datepicker.css';
 
 const AddDocAppointment = () => {
   const [allOutPatients, setAllOutPatients] = useState([]);
   const [allDOctors, setAllDOctors] = useState([]);
+  const [startDate, setStartDate] = useState(new Date());
+  const [value, onChange] = useState("10:00");
+  const [selectedTime, setSelectedTime] = useState(null);
+  const [showPatient, setShowPatient] = useState(true);
+  const [isFocusedP, setIsFocusedP] = useState(false);
+  const [isFocusedD, setIsFocusedD] = useState(false);
 
   // To Get all outPatient
   const API_URL = "http://localhost:3001/getOutPatientsDetails";
@@ -20,10 +30,31 @@ const AddDocAppointment = () => {
     setAllDOctors(data);
   };
 
+  const handleFocusPatient = () => {
+    setIsFocusedP(true);
+  };
+
+  const handleBlurPatient = () => {
+    setIsFocusedP(false);
+  };
+
+  const handleFocusDoctor = () => {
+    setIsFocusedD(true);
+  };
+
+  const handleBlurDoctor = () => {
+    setIsFocusedD(false);
+  };
+
   useEffect(() => {
     fetchData();
     fetchDoctorsData();
   }, []);
+
+  useEffect(() => {
+    console.log("the date", startDate);
+  }, [startDate]);
+
   return (
     <div className="app__addAppointment">
       <h2 className="page-title">ADD DOCTOR APPOINTMENT</h2>
@@ -31,7 +62,7 @@ const AddDocAppointment = () => {
         <div className="appScheduling-container">
           <div className="details-title">
             <h4>Appointment Details</h4>
-            <div className="divider" />
+            {/* <div className="divider" /> */}
           </div>
           <form>
             <div className="input-field doctor-types">
@@ -42,6 +73,8 @@ const AddDocAppointment = () => {
                   id="patientID"
                   // value={appointmentInfos.doctorID}
                   // onChange={handleOnChangeAppointment}
+                  onFocus={handleFocusPatient}
+                  onBlur={handleBlurPatient}
                   required
                 >
                   <option required value="">
@@ -64,6 +97,8 @@ const AddDocAppointment = () => {
                   // value={appointmentInfos.doctorID}
                   // onChange={handleOnChangeAppointment}
                   required
+                  onFocus={handleFocusDoctor}
+                  onBlur={handleBlurDoctor}
                 >
                   <option required value="">
                     Select a doctor ID
@@ -77,25 +112,132 @@ const AddDocAppointment = () => {
 
                 <span
                   // onClick={handleShowDocDetailsTable}
-                  className="btn-seeAll"
+                  className="btn-docSchedule"
                 >
-                  See All
+                  Doctors Schedule
                 </span>
               </div>
             </div>
+
             <div className="input-field">
-              <label htmlFor="gender"> Gender:</label>
-              <Input
-                placeholder="Gender"
-                name="gender"
-                inputDisabled="true"
-                // value={lastElement ? lastElement.timeOut : ""}
-              />
+              <label htmlFor="gender"> Appointment Date:</label>
+              <div>
+                <DatePicker
+                  selected={startDate}
+                  onChange={(date) => setStartDate(date)}
+                />
+              </div>
             </div>
-            s
+            <div className="input-field">
+              <label htmlFor="gender"> Appointment Time:</label>
+              <div>
+                <DatePicker
+                  selected={selectedTime}
+                  onChange={(time) => setSelectedTime(time)}
+                  showTimeSelect
+                  showTimeSelectOnly
+                  timeIntervals={10}
+                  dateFormat="h:mm aa"
+                  timeCaption="Time"
+                />
+              </div>
+            </div>
           </form>
+
+          <div className="container-view-appoint-btn">
+            <ButtonAction
+              iconName="close"
+              btnName="Save Appointment"
+              color="green"
+              buttonType="button"
+              // onClick={handleCloseScheduling}
+            />
+            <ButtonAction
+              iconName="close"
+              btnName="Close"
+              color="red"
+              buttonType="button"
+              // onClick={handleCloseScheduling}
+            />
+          </div>
         </div>
-        <aside className="container-view-appoint"></aside>
+
+        <aside className="container-view-appoint">
+          {/* empty span */}
+          {!isFocusedP && !isFocusedD && <span />}
+
+          {/* Table for  Out patient details */}
+          {isFocusedP && (
+            <table className="table-view-app">
+              <thead>
+                <tr>
+                  <th>Patient ID</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Gender</th>
+                  <th>Address</th>
+                </tr>
+              </thead>
+              <tbody>
+                {allOutPatients.map((patient, index) => {
+                  return (
+                    <tr className="table-view-app-row" key={index}>
+                      <td>{patient.patientID}</td>
+                      <td>{patient.firstName}</td>
+                      <td>{patient.lastName}</td>
+                      <td>{patient.gender}</td>
+                      <td>{patient.address}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
+
+          {/* Table for  Doctors details */}
+          {isFocusedD && (
+            <table className="table-view-app">
+              <thead>
+                <tr>
+                  <th>Doctor ID</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Specialization</th>
+                  <th>Qualification</th>
+                </tr>
+              </thead>
+              <tbody>
+                {allDOctors.map((doctor, index) => {
+                  return (
+                    <tr className="table-view-app-row" key={index}>
+                      <td>{doctor.doctorID}</td>
+                      <td>{doctor.doctorFN}</td>
+                      <td>{doctor.doctorLN}</td>
+                      <td>{doctor.Specialization}</td>
+                      <td>{doctor.Qualifications}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
+          <div className="container-view-appoint-btn">
+            <ButtonAction
+              iconName="close"
+              btnName="Ok"
+              color="blue"
+              buttonType="button"
+              // onClick={handleCloseScheduling}
+            />
+            <ButtonAction
+              iconName="close"
+              btnName="Cancel"
+              color="red"
+              buttonType="button"
+              // onClick={handleCloseScheduling}
+            />
+          </div>
+        </aside>
       </div>
     </div>
   );
