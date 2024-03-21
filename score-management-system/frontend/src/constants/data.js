3d9cb3cdab0066c8cbf9f0efdcfe54a48b1f95aa
William Hortone <101454117+William-Hortone@users.noreply.github.com>
2024-03-21 14:08:12 +0800

    updated the navbar data

---
+++ score-management-system/frontend/src/constants/data.js
@@ -0,0 +1,14 @@
+const scoreInfos = [
+  { title: "Add score", link: "/score/addScore" },
+  { title: "Edit score", link: "/score/addScore" },
+  { title: "View all scores", link: "/score/viewScore" },
+  { title: "Delete score", link: "/score/addScore" },
+];
+const studentInfos = [
+  { title: "Add student", link: "/student/addStudent" },
+  { title: "Edit student infos", link: "/student/editStudent" },
+  { title: "View all Students", link: "/student/viewStudents" },
+  { title: "Delete student", link: "/student/deleteStudent" },
+];
+
+export { scoreInfos, studentInfos };
