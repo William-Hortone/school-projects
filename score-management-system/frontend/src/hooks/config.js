c9630ba02d7f2fbafae30aa773f1bda41a5882ac
William Hortone <101454117+William-Hortone@users.noreply.github.com>
2024-04-24 19:16:56 +0800

    added the score management system

--- score-management-system/frontend/src/hooks/config.js
+++
@@ -1,3 +0,0 @@
-const BASE_URL = "https://score-management-api.vercel.app/api/";
-// p;
-export default BASE_URL;
---
+++ score/server/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const errorHandler = require("./middleware/errorHandling");
+const authRouter = require("./routes/auth");
+const userRouter = require("./routes/user");
+const studentRouter = require("./routes/student");
+const scoreRouter = require("./routes/score");
+const cors = require("cors");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.error("Database connection error:", err));
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(cors()); // Uncomment and configure if needed
+// app.use(
+//   cors({
+//     origin: ["https://score-management-client.vercel.app"],
+//     methods: ["GET", "POST", "PUT"],
+//     credentials: true,
+//   })
+// );
+
+app.use("/api/", authRouter);
+app.use("/api/users/", userRouter);
+app.use("/api/student/", studentRouter);
+app.use("/api/score/", scoreRouter);
+
+app.get("/", (req, res) => res.send("William Hortone!"));
+
+app.use(errorHandler); // Error handling middleware at the end
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App listening on port ${port}!`));
