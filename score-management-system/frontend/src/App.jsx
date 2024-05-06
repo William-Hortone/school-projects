1ba597ed6ca9dd4409b21a472ceecb083b510b64
William Hortone <101454117+William-Hortone@users.noreply.github.com>
2024-05-07 01:50:40 +0800

    updated the login section

---
+++ score-management-system/frontend/src/App.jsx
@@ -0,0 +1,68 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import { Login, Register } from "./components";
+import {
+  AddStudent,
+  DeleteStudent,
+  EditStudent,
+  ViewStudents,
+  AddScore,
+  AddStudentScore,
+  ViewStudentScore,
+  ViewScore,
+  EditScore,
+} from "./containers";
+import { AuthProvider } from "./hooks/AuthContext";
+// import StudentHome from "./../../../../score/client/src/pages/studentHome/StudentHome";
+import { About, Connection, Home, Score, Student, Welcome } from "./pages";
+
+function App() {
+  return (
+    <>
+      <ToastContainer />
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            {/* <Route path="/StudentHome" element={<StudentHome />} /> */}
+            <Route path="/connection" element={<Connection />}>
+              <Route path="/connection/register" element={<Register />} />
+              <Route path="/connection/login" element={<Login />} />
+            </Route>
+            <Route path="/student" element={<Student />}>
+              <Route path="/student/addStudent" element={<AddStudent />} />
+              <Route path="/student/editStudent" element={<EditStudent />} />
+              <Route path="/student/viewStudents" element={<ViewStudents />} />
+              <Route
+                path="/student/deleteStudent"
+                element={<DeleteStudent />}
+              />
+            </Route>
+            <Route path="/score" element={<Score />}>
+              <Route path="/score/addScore" element={<AddScore />} />
+
+              <Route
+                path="/score/addStudentScore"
+                element={<AddStudentScore />}
+              />
+
+              <Route path="/score/viewScore" element={<ViewScore />} />
+              <Route
+                path="/score/viewStudentScore"
+                element={<ViewStudentScore />}
+              />
+              <Route path="/score/editScore" element={<EditScore />} />
+            </Route>
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            {/* <Route path="/addScore" element={<AddScore />} /> */}
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
