10cc3d110c2c790098f825fd594a0709418e6f65
William Hortone <101454117+William-Hortone@users.noreply.github.com>
2024-06-28 05:02:02 +0800

    updated the dependencies

--- score/server/index.js
+++ score/server/index.js
@@ -29,8 +29,8 @@ app.use(cors());
 
 // Middleware for handling OPTIONS requests
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  // res.header("Access-Control-Allow-Origin", "https://score-client.vercel.app");
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://score-client.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
