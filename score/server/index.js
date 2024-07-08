c15e1e13c483d85767b6a61e9dd64281ac444d62
William Hortone <101454117+William-Hortone@users.noreply.github.com>
2024-07-08 21:48:39 +0800

    Updated Cors

--- score/server/index.js
+++ score/server/index.js
@@ -16,31 +16,20 @@ mongoose
   .then(() => console.log("Database connected"))
   .catch((err) => console.error("Database connection error:", err));
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: ["https://score-client.vercel.app"],
-//     methods: ["GET", "POST", "PUT"],
-//     credentials: true,
-//   })
-// );
+  app.use(express.json({ limit: "10mb" }));
+  app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
-// Middleware for handling OPTIONS requests
-app.use((req, res, next) => {
-  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Origin", "https://score-client.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+  app.use(errorHandler);
 
+// Enable CORS for all routes
+app.use(cors({
+  origin: "https://score-client.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true 
+}));
+
+// Routes
 app.use("/api/", authRouter);
 app.use("/api/users/", userRouter);
 app.use("/api/student/", studentRouter);
@@ -48,7 +37,6 @@ app.use("/api/score/", scoreRouter);
 
 app.get("/", (req, res) => res.send("William Hortone!"));
 
-app.use(errorHandler);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App listening on port ${port}!`));
